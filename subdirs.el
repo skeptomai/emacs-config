4b9970167ee12c401a3759a868294a4357e6e40c
Christopher Brown <cb@opscode.com>
2010-03-09 09:36:26 -0800

    ok, actually re-add erlware

--- subdirs.el
+++ subdirs.el
@@ -1,5 +1,5 @@
 (let* ((init-path (concat *home-elisp-path* "/my-elisp"))
-       (my-elisps '("baseline" "keys" "fns" "erc" "org" "bm" "tramp" "cucumber"))
+       (my-elisps '("baseline" "keys" "fns" "erc" "org" "bm" "tramp" "erlware" "cucumber"))
        (default-directory *home-elisp-path*))
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path))
