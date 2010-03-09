ff50d566297efee48528b9814d75accead370ddc
Christopher Brown <cb@opscode.com>
2010-03-08 21:29:15 -0800

    removed extraneous git, java, ruby & python settings

--- subdirs.el
+++ subdirs.el
@@ -1,5 +1,5 @@
 (let* ((init-path (concat *home-elisp-path* "/my-elisp"))
-       (my-elisps '("baseline" "keys" "fns" "erc" "git" "java" "org" "bm" "python" "ruby" "tramp" "xml" "erlware" "cucumber"))
+       (my-elisps '("baseline" "keys" "fns" "erc" "org" "bm" "tramp" "erlware" "cucumber"))
        (default-directory *home-elisp-path*))
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path))
