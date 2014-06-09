e07ff9852284a3afd8f4854bbf3710ac0739f92b
Christopher Brown <cb@skeptomai.com>
2014-06-09 04:59:38 +0000

    first pass at cleanup

--- subdirs.el
+++ subdirs.el
@@ -1,8 +1,9 @@
 (let* ((init-path (concat *home-elisp-path* "/my-elisp"))
        (my-elisps 
-        '("haskell" "ruby" "scheme" "haml" "baseline" "keys" "fns" "erc" "org" "bm" "tramp" "erlware" "cucumber" "textile-mode" "textile" "http-twiddle" "opscode-sign" "dash-at-point" "go-mode.el" "gocode"))
+	'("baseline" "keys" "org" "textile-mode" "bm"))
        (default-directory *home-elisp-path*))
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path))
   (mapcar (lambda (x) (load-file (expand-file-name (concat "my-" x ".el") init-path))) my-elisps))
 
+;        '("haskell" "ruby" "scheme" "haml" "baseline" "keys" "fns" "erc" "org" "bm" "tramp" "erlware" "cucumber" "textile-mode" "textile" "http-twiddle" "opscode-sign" "dash-at-point" "go-mode.el" "gocode"))
