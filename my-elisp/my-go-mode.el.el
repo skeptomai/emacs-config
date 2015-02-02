62ba6aa22126fa32e6f3e4dca489bb7148ae414d
Christopher Brown <cbrown@fastly.com>
2015-02-02 14:53:33 -0800

    add before-save-hook for gofmt

--- my-elisp/my-go-mode.el.el
+++ my-elisp/my-go-mode.el.el
@@ -2,7 +2,7 @@
 (add-to-list 'load-path (concat *home-elisp-path* "/go-mode.el/"))
 (setenv "GOPATH" "/Users/cb/Projects/gopath")
 (setq exec-path (append exec-path (list (expand-file-name "/Users/cb/Projects/gopath/bin"))))
-
+(add-hook 'before-save-hook #'gofmt-before-save)
 (require 'go-mode-load)
 (require 'go-flymake)
 (require 'go-flycheck)
