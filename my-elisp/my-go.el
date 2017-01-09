24195de59729f836cb6a9cc5fe5398a7120a7a7d
Christopher Brown <cb@skeptomai.com>
2017-01-08 20:00:59 -0800

    remove merge errors

--- my-elisp/my-go.el
+++ my-elisp/my-go.el
@@ -1,8 +1,5 @@
-<<<<<<< HEAD:my-elisp/my-go-mode.el
 (setenv "GOPATH" (expand-file-name (concat (getenv "HOME") "/Projects/gopath")))
 (setenv "GOROOT" "/usr/local/go")
-=======
->>>>>>> 681c9570fba187d0dfd22dba397ed83513c31436:my-elisp/my-go.el
 (setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name (concat (getenv "GOROOT") "/bin"))))
 (add-to-list 'load-path (expand-file-name (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake")))
 (setq exec-path (append exec-path (list (expand-file-name (concat (getenv "GOPATH") "/bin")))))
